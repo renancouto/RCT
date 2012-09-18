c0a19b11e5eb506ff8caa2aa70e0fed498b236d1
Renan Couto <renancouto@gmail.com>
2012-09-17 22:20:07 -0300

    Carregamento de fontes via script

--- header.php
+++ header.php
@@ -2,13 +2,25 @@
 <html class="no-js" lang="en">
 
 <head>
-	<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic,700italic|Exo:200,500,700,200italic,500italic,700italic' rel='stylesheet' type='text/css'>
-	
+	<script>
+	WebFontConfig = {
+		google: { families: [ 'Source+Sans+Pro:400,700,400italic,700italic:latin', 'Exo:200,500,700,200italic,500italic,700italic:latin' ] }
+	};
+	(function() {
+		var wf = document.createElement('script');
+			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') + '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+			wf.type = 'text/javascript';
+			wf.async = 'true';
+		var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(wf, s);
+	})();
+	</script>
+
 	<meta charset="<?php bloginfo('charset'); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	
+
 	<?php if (is_search()) { ?>
-	<meta name="robots" content="noindex, nofollow" /> 
+	<meta name="robots" content="noindex, nofollow" />
 	<?php } ?>
 
 	<title>
@@ -20,7 +32,7 @@
 			  elseif (is_search()) {
 				 echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
 			  elseif (!(is_404()) && (is_single()) || (is_page() && !is_front_page())) {
-				 wp_title(''); echo ' - '; }		      
+				 wp_title(''); echo ' - '; }
 			  elseif (is_404()) {
 				 echo 'Not Found - '; }
 			  if (is_home()) {
@@ -31,7 +43,7 @@
 				 echo ' - page '. $paged; }
 		   ?>
 	</title>
-	
+
 	<meta name="title" content="<?php
 			  if (function_exists('is_tag') && is_tag()) {
 				 single_tag_title("Tag Archive for &quot;"); echo '&quot; - '; }
@@ -52,15 +64,15 @@
 		   ?>">
 	<meta name="description" content="<?php bloginfo('description'); ?>">
 	<meta name="author" content="Renan Couto">
-	
-	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">		 
+
+	<link rel="shortcut icon" href="<?php bloginfo('template_directory'); ?>/_/img/favicon.ico">
 	<link rel="apple-touch-icon" href="<?php bloginfo('template_directory'); ?>/_/img/apple-touch-icon.png">
 
 	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
-	
+
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/libs/modernizr-2.5.3.min.js"></script>
 	<script src="<?php bloginfo('template_directory'); ?>/_/js/libs/browsr-0.2.js"></script>
-	
+
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
 	<?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
@@ -80,5 +92,5 @@
 	</script>
 </head>
 
-<body <?php body_class(); ?>>	
+<body <?php body_class(); ?>>
 	<div id="wrapper" class="main-wrapper">
